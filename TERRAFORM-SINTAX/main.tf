fd821a9936e413ac32ab971ba6de7b864c3e8c70
“Konstantin <vitkinkn@mail.ru>
2022-06-26 23:10:39 +0700

    ADDED TASK_7_2_2

--- TERRAFORM-SINTAX/main.tf
+++ TERRAFORM-SINTAX/main.tf
@@ -11,10 +11,50 @@ provider yandex {
   service_account_key_file = "key.json"
   cloud_id  = "b1gjj00oe6mlvmeoj0h6"
   folder_id = "b1g3r78e9ad9be8bcmdr"
-  zone      = "default-ru-central1-a"
+  zone      = "ru-central1-a"
 }
 
-resource yandex_compute_image ubu-img {
-  name          =  "centos-7-base"
-  source_image  = "fd8fphmm1jud0338snf9"
+data "yandex_compute_image" "ubuntu" {
+  family = "ubuntu-2004-lts"
+}
+
+resource "yandex_vpc_network" "net" {
+  name = "net"
+}
+
+resource "yandex_vpc_subnet" "subnet" {
+  name           = "subnet"
+  network_id     = resource.yandex_vpc_network.net.id
+  v4_cidr_blocks = ["10.2.0.0/16"]
+  zone           = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "vm" {
+  name        = "newinstance"
+  hostname    = "newinstance.local"
+  platform_id = "standard-v1"
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 100
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.id
+      type     = "network-hdd"
+      size     = "20"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet.id
+    nat       = true
+    ipv6      = false
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
 }
